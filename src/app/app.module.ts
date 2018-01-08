c6a9905903c39608652dd892f3bdf389c25f3083
Michael Graßmann <michael.grassmann@dekra.com>
2018-01-08 13:17:47 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { IonicStorageModule } from '@ionic/storage';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,189 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { ToastController } from 'ionic-angular';
+
+
+interface  todotype {
+  todo: string;
+  content: string;
+  status: boolean;
+}
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ public  todotxt: string;
+  public todolst: todotype[] = [{todo:'Task1',content:'',status:false}];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController,private storage: Storage,
+              public toastCtrl: ToastController) {
 
+    // Load Tasks
+    this.loadStorage(false)
+
+    if (this.todolst.length < 2) {
+      this.todolst = [{todo:'Task1',content:'',status:false},{todo:'Task2',content:'',status:false},
+        {todo:'Task3',content:'',status:false},{todo:'Task4',content:'',status:false}];
+      this.saveStorage(false);
+    }
+  }
+
+  saveStorage(withMsg: boolean): void {
+    try {
+      this.storage.set('tasks',JSON.stringify(this.todolst));
+      if (withMsg) {
+        let toast = this.toastCtrl.create({
+          message: 'saved ' + this.todolst.length + ' entries',
+          duration: 2000
+        });
+        toast.present();
+      }
+    } catch (e) {
+      console.log('save entry error');
+    }
+
+  }
+
+  loadStorage(withMsg: boolean): void {
+    try {
+      this.storage.get('tasks').then(tlist => {
+         this.todolst = JSON.parse(tlist);
+      });
+      if (withMsg) {
+        let toast = this.toastCtrl.create({
+          message: 'load ' + this.todolst.length + ' entries',
+          duration: 2000
+        });
+        toast.present();
+      }
+    } catch (e) {
+      console.log('no entries found!');
+    }
+  }
+
+  public doaddtask(todo: string) {
+    var entry: String;
+    var newEntry: todotype = {todo:'-',content:'-',status:false};
+
+    if (todo==null) return;
+    entry=todo.toString();
+    todo=entry.trim();
+    if(todo=='') return;
+
+    newEntry.todo=todo;
+    newEntry.content='-';
+    newEntry.status=false;
+    this.todolst.push(newEntry);
+    this.todotxt='';
+  }
+
+  public clickItem(item: any) {
+    this.presentAlert3(item);
+  }
+
+
+  public delItem(item: any) {
+
+    // Löschen eine Items
+    this.presentAlert(item);
+
+  }
+
+  public doDelItem(item: any) {
+    this.todolst.splice(item,1);
+  }
+
+  public doclearlist() {
+    this.presentAlert2();
+
+  }
+
+  public doDelAllItems() {
+    this.todolst.splice(0)
+  }
+
+  presentAlert(item: any) {
+    let alert = this.alertCtrl.create({
+      title: 'ToDo-List...',
+      subTitle: 'delete Task ['+this.todolst[item]+'] ?',
+      buttons: [
+        {
+          text: 'Yes',
+          handler: () => {
+            this.doDelItem(item);
+          }
+        },
+        {
+          text: 'No',
+          handler: () => {
+            return;
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  presentAlert2() {
+    let alert = this.alertCtrl.create({
+      title: 'ToDo-List...',
+      subTitle: 'clear List ?',
+      buttons: [
+        {
+          text: 'Yes',
+          handler: () => {
+            this.doDelAllItems();
+            this.todolst.splice(0,this.todolst.length);
+            this.saveStorage(false);
+          }
+        },
+        {
+          text: 'No',
+          handler: () => {
+            return;
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  presentAlert3(item: any) {
+    var newEntry: todotype = {todo:'-',content:'-',status:false};
+
+
+    let alert = this.alertCtrl.create({
+      title: 'ToDo-Liste',
+      subTitle: 'this Entry...',
+      buttons: [
+        {
+          text: 'Edit',
+          handler: () => {
+            newEntry=this.todolst[item];
+            // Edit
+
+            this.navCtrl.push('DetailsPage', { param1: newEntry, param2: '' });
+          }
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            this.doDelItem(item);
+          },
+        },
+        {
+          text: 'Cancel',
+          handler: () => {
+            return;
+          },
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
